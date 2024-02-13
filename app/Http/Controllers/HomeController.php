8055153bbd10458468cdefc08e334c6402a3164c
Asrafuzzaman <asrfuzzaman2379@gmail.com>
2024-02-14 01:10:11 +0600

    new

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,7 +36,7 @@ class HomeController extends Controller
      */
     public function adminHome(): View
     {
-        return view('adminHome');
+        return view('admin.master');
     }
 
     /**
