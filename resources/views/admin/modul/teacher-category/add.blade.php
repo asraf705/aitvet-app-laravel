fc6067d7b67493a9aa8c83f0ee3e66f2e4cc0a13
Asrafuzzaman <asrfuzzaman2379@gmail.com>
2024-02-15 08:41:15 +0600

    new

--- resources/views/admin/modul/teacher-category/add.blade.php
+++ resources/views/admin/modul/teacher-category/add.blade.php
@@ -7,7 +7,7 @@
 
 @section('body')
     <section class="section">
-        <div class="section-body"
+        <div class="section-body">
             <div class="row">
                 <div class="col-12 col-md-12 col-lg-12">
 
